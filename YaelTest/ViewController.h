1c08e1852c058b1d785a678a9b15b8896e5447b6
YaelBuchman <y.bochman@applicaster.com>
2014-04-06 12:34:57 +0300

    Yael added a new project

---
+++ YaelTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YaelTest
+//
+//  Created by Yael Buchman on 4/6/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
